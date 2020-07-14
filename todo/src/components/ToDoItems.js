93b88eacb5d21bd471623e0b7fe59b59f68cbb0d
Eorium <travisgent312@gmail.com>
2020-07-13 21:44:38 -0600

    add toggle function

--- todo/src/components/ToDoItems.js
+++ todo/src/components/ToDoItems.js
@@ -5,6 +5,8 @@ import { reducer, initialState } from "../reducers/Reducer";
 function ToDoItmes() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [newToDoText, setNewToDoText] = useState("");
+  const [boolVal, setBoolVal] = useState(false);
+  console.log(state);
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -14,10 +16,15 @@ function ToDoItmes() {
     setNewToDoText(event.target.value);
   }
 
+  const toggleItem = event => {
+    event.preventDefault();
+    setBoolVal(!boolVal);
+  }
+
   return (
     <div>ToDo Items:
       {state.map(item => (
-            <div className="listItem"> {item.item}</div>
+            <div className="listItem" onClick={() => toggleItem}>{item.item}</div>
       ))}
       <form onSubmit={handleSubmit}>
         <input type="text" name="item" value={newToDoText} onChange={handleChanges}/>
